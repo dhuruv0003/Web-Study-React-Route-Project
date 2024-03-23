414969f3f8ea7eae6d5f524798ffb38caac66ebe
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-23 16:11:37 +0530

    "new commit"

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,30 @@
+import { Link,NavLink } from "react-router-dom"
+import logo from "../assets/Logo.svg"
+
 export default function Navbar(){
     return (
-        <div>
-            hello
+        <div className="flex gap-5">
+            <Link to="/">
+                <img src={logo} alt="logo" width={160} height={32} loading="lazy" />
+            </Link>
+            <nav>
+                <ul className="flex gap-3">
+                    <li><Link to="/">Home</Link></li>
+                    <li> <NavLink to="/">About</NavLink></li>
+                    <li><NavLink to="/">Contact</NavLink></li>
+                </ul>
+            </nav>
+
+        {/* Creating buttons Login - Signup - LogOut - Dashboard */}
+
+            <div className="flex gap-3">
+                <NavLink to="/login"><button>Login</button></NavLink>
+                <NavLink to="/signup"><button>SignUp</button></NavLink>
+                <NavLink to="/"><button>Logout</button></NavLink>
+                <NavLink to="/dashboard"><button>DashBoard</button></NavLink>
+
+            </div>
+
         </div>
     )
 } 

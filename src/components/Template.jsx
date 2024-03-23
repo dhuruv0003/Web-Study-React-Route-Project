2b3cda0f1a904fb91a39aa9b099d276c6e6da5f0
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-23 18:19:53 +0530

    "new commit"

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,14 +1,36 @@
+import frameimage from "../assets/frame.png"
+
 export default function Template({ title, desc1, desc2, image, formtype, setloggedin }) {
     // this template is created in order to have similar components for both login ansd signup page.  
     return (
         <div className="">
-            <div className="">
+            <div className="Left-Section">
                 <h1>{title}</h1>
                 <p>
                     <span>{desc1}</span>
                     <span>{desc2}</span>
                 </p>
+                {
+                    formtype === "signup" ? (<Signupform />) : (<LoginForm />)
+                }
 
+                <div className="">
+                    <div></div>
+                    <p> OR </p>
+                    <div></div>
+                </div>
+
+                 <button>Sign in With Google</button>
+            </div>
+            <div className="right-section">
+                <img src={frameimage} alt="pattern image" 
+                width={558}
+                height={504}
+                loading="lazy"/>
+                <img src={image} alt="pattern image" 
+                width={558}
+                height={490}
+                loading="lazy"/>
             </div>
         </div>
     )

e729a363e9f0b7bfcda6dc0519adbd1440d7c95e
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-26 18:38:34 +0530

    "new commit"

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,9 @@
+export default function loginform(){
+    return (
+        <form action="">
+            <label htmlFor="email">
+                <p>Email Address <sup>*</sup></p>
+            </label>
+        </form>
+    )
+} 
---
+++ src/components/Signupform.jsx
@@ -0,0 +1,9 @@
+export default function Signupform(){
+    return (
+        <form action="">
+            <label htmlFor="email">
+                <p>Email Address <sup>*</sup></p>
+            </label>
+        </form>
+    )
+} 

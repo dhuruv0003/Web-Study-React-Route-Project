3289a343427e8fe6796be12e331a914cf8165b65
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-30 01:13:47 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from './pages/Dashboard.jsx'
 import Navbar from './components/navbar.jsx'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import PrivateRoute from './components/PrivateRoute.jsx'
 
 function App() {
 
@@ -14,15 +14,26 @@ function App() {
 
   return (
     <div className="w-screen h-screen  bg-[#fdf0d5]">
-      <Navbar isloggedin={isloggedin} setloggedin={setloggedin}/>
+      <Navbar isloggedin={isloggedin} setloggedin={setloggedin} />
 
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
 
         <Route path='/login' element={<Login setloggedin={setloggedin}></Login>}></Route>
 
-        <Route path='/signup' element={<Signup  setloggedin={setloggedin}></Signup>}></Route>
-        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/signup' element={<Signup setloggedin={setloggedin}></Signup>}></Route>
+
+        {/* <PrivateRoute/> :- routes that require authentication before granting access to specific components or pages. */}
+
+        {/* Here we want to access dashborad only when we are logged in. Thatswhy we will keep dashboard inside private route tab */}
+
+
+        <Route path='/dashboard' element={
+          <PrivateRoute isloggedin={isloggedin} >
+            {/* note here Dashboard is the children of privateroute   */}
+            <Dashboard></Dashboard>
+          </PrivateRoute>
+        }></Route>
 
       </Routes>
     </div>
---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom"
+
+export default function PrivateRoute({isloggedin,children}){
+
+    if(isloggedin){
+        return children
+    //  Matlab agar logged in hai toh dashborad par navigate kar saketke hai else apis login page par redirect ho jao   
+    }
+    else{
+       return <Navigate to="/login"/>
+    }
+
+}
